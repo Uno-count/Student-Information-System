26c5ec467cfee86d78651a785925bc51d3c17ede
Uno-count <paonemorfi@gmail.com>
2021-12-11 02:15:56 +0800

    minor changes enrollment form

--- student_Section/Student_enrollment.php
+++ student_Section/Student_enrollment.php
@@ -30,7 +30,11 @@ if (isset($_SESSION['studno'])) {
             </nav>
             <div class="selector">
                 <div id="selectField">
-                    <p id="selectext">2019216</p>
+                    <p class="select-text"><?php if (isset($_SESSION['studno'])) {
+                                                echo $_SESSION['studno'][0]; //student number
+
+                                            }
+                                            ?>-N</p>
                     <img src="https://img.icons8.com/ios/50/ffffff/expand-arrow--v1.png" id="arrowIcon" />
                 </div>
                 <ul id="list" class="hide">
@@ -40,7 +44,7 @@ if (isset($_SESSION['studno'])) {
                     </li>
                     <li class="options">
                         <img src="https://img.icons8.com/external-prettycons-lineal-prettycons/49/000000/external-exit-essentials-prettycons-lineal-prettycons.png" />
-                        <a href="../student-logout.php">Log Out</a>
+                        <a href="student-logout.php">Log Out</a>
                     </li>
                 </ul>
             </div>
@@ -62,19 +66,36 @@ if (isset($_SESSION['studno'])) {
                     <div class="enrollment-content">Registration (SY. 2021-2022 First Semester)</div>
                 </div>
                 <div id="Status">
-                    <div class="enrollment-content">Status: Regular</div>
+                    <div class="enrollment-content">
+                        <p><?php if (isset($_SESSION['studno'])) {
+                                echo "Status: ";
+                                echo $_SESSION['studno'][9]; //status
+
+                            }
+                            ?> </p>
+                    </div>
                 </div>
                 <div id="Section">
-                    <div class="enrollment-content">Section: BSCS 3C</div>
+                    <div class="enrollment-content">
+                        <p><?php if (isset($_SESSION['studno'])) {
+                                echo "Course: ";
+                                echo $_SESSION['studno'][4]; //year
+                                echo " ";
+                                echo $_SESSION['studno'][6]; //section
+                            }
+                            ?> </p>
+                    </div>
                 </div>
                 <div id="Online-Enrollment">
                     <div class="enrollment-content">Online Enrollment is not yet open.</div>
                 </div>
             </section>
+
         </main>
-        <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 1440 320">
-            <path fill="#00cba9" fill-opacity="0.8" d="M0,32L40,48C80,64,160,96,240,101.3C320,107,400,85,480,106.7C560,128,640,192,720,197.3C800,203,880,149,960,149.3C1040,149,1120,203,1200,224C1280,245,1360,235,1400,229.3L1440,224L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
-        </svg>
+
+
+        <div class="footer">
+        </div>
         <script src="js/navbar.js"></script>
         <script>
             var selectField = document.getElementById("selectField");
